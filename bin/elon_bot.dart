26daa72d8d8698f0ed0019e8ce14e72c6f4a5fcc
Xushnudbek Abdusamatov <xuwnudbek266@gmail.com>
2024-03-11 11:26:30 +0500

    asdasdasd

---
+++ bin/elon_bot.dart
@@ -0,0 +1,22 @@
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+
+// ignore: non_constant_identifier_names
+String TOKEN = '5830866035:AAHlnvAxf-vv_Hi-E9iJelX7c8IikVKk4tw';
+
+void main() async {
+  try {
+    var telegram = await Telegram(TOKEN).getMe();
+
+    var teleDart = TeleDart(TOKEN, Event(telegram.username!));
+    var res = await teleDart.deleteWebhook();
+    print("Delete Webhook: $res");
+    teleDart.start();
+
+    teleDart.onMessage().listen((event) {
+      teleDart.sendMessage(event.chat.id, event.text!);
+    });
+  } catch (e) {
+    print(e.toString());
+  }
+}
